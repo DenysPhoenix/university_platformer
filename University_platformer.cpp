ede105c6ac587ec6a2213b0526bfa67944052670
DenysPhoenix <denysdenysenko18@gmail.com>
2025-05-20 13:57:51 +0200

    hello world sprawdzenie polaczenia

--- University_platformer.cpp
+++ University_platformer.cpp
@@ -2,9 +2,11 @@
 #include "SFML\Graphics.hpp"
 #include "SFML\Window.hpp"
 #include "SFML\System.hpp"
+
+using namespace std;
 int main()
 {
-
+    cout << "hello world test polaczenia";
     return 0;
 }
 
